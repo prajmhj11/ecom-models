af5926782db7c59ff1a86f113d3de7d203761a54
prajmhj11 <praj.mhj11@gmail.com>
2020-11-04 09:33:21 +0545

    add category model

---
+++ src/CategoryProduct.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Laravel\SharedModels;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryProduct extends Model
+{
+    use HasFactory;
+    protected $table = 'category_product';
+}
